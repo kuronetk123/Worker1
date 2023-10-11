c497eaa226dc0407ce4f5fa6681d9de34b436590
ECCHAN <126219665+kuronetk123@users.noreply.github.com>
2023-10-11 10:50:08 +0700

    first

---
+++ src/repository/WorkerRepository.java
@@ -0,0 +1,34 @@
+package repository;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import dataAccess.WorkerDao;
+import java.util.Hashtable;
+import java.util.List;
+import model.SalaryHistory;
+import model.Worker;
+
+/**
+ *
+ * @author DiepTCNN
+ */
+public class WorkerRepository implements IWorkerRepository {
+
+    @Override
+    public void addWorker(Hashtable<String, Worker> workers) {
+        WorkerDao.Instance().addWorker(workers);
+    }
+
+    @Override
+    public void changeSalary(Hashtable<String, Worker> workers, List<SalaryHistory> salaryHistorys, String status) {
+        WorkerDao.Instance().changeSalary(workers, salaryHistorys, status);
+    }
+
+    @Override
+    public void displaySalary(List<SalaryHistory> salaryHistorys) {
+        WorkerDao.Instance().displayInfomationSalary(salaryHistorys);
+    } 
+}
---
+++ src/workermanagement1/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package workermanagement1;
+import controller.WorkerManagement;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Main {
+    public static void main(String[] args) {
+        new WorkerManagement().run();
+    }
+}
